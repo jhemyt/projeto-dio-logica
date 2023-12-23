cce5ced2117bb58d90384b4a3c834f3a6f474628
jhemyt <lokoporskate40@gmail.com>
2023-12-22 22:15:48 -0300

    nome e xp status

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-const age = 18 
-let name = " matheus "
+let nameHero = 'Gamesh'
+let xpHero = 6000;
 
-console.log("Você tem " + age + "," + name )
+switch(true) {
+   case xpHero < 1000:
+         console.log('O herói de nome ' + nameHero + ' está no nível de' + ' Ferro' )
+             break;           
+   case xpHero >= 1001 && xpHero <= 2000:
+         console.log('O herói de nome ' + nameHero + ' está no nível de' + ' Bronze' ) 
+             break; 
+   case xpHero >= 2001 && xpHero <= 5000:
+         console.log('O herói de nome ' + nameHero + ' está no nível de' + ' Prata' )
+             break;
+   case xpHero >= 5001 && xpHero <= 7000:
+         console.log('O herói de nome ' + nameHero + ' está no nível de' + ' Ouro' )
+             break;
+   case xpHero >= 7001 && xpHero <= 8000:
+         console.log('O herói de nome ' + nameHero + ' está no nível de' + ' Platina' )
+             break;
+    case xpHero >= 8001 && xpHero <= 9000:
+           console.log('O herói de nome ' + nameHero + ' está no nível de' + ' Ascendente' )
+             break;
+     case xpHero >= 9001 && xpHero <= 10000:
+          console.log('O herói de nome ' + nameHero + ' está no nível de' + ' Imortal' )
+             break;
+    case xpHero > 10001:
+         console.log('O herói de nome ' + nameHero + ' está no nível de' + ' Radiante' )
+}
+
+
